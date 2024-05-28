344c7c060bff270ea358298a8339fb4ff9872a1d
Omar Gamal Fathy | Ejada Systems Ltd. <ogfathy@ejadacairo.local>
2024-05-28 11:01:18 +0300

    Add new files to main branch

---
+++ src/main/java/com/example/learnwithgpt/LearnwithgptApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnwithgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnwithgptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnwithgptApplication.class, args);
+	}
+
+}
